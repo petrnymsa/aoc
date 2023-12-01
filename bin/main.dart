3263d00536087898df949395b9516a5303334cee
Petr Nymsa <nymsa.petr@outlook.cz>
2023-12-01 21:06:20 +0100

    day 1

---
+++ bin/main.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:aoc_2023/01.dart';
+
+void main(List<String> arguments) async {
+  List<String> lines = [];
+
+  if (arguments.isNotEmpty) {
+    lines = await File(arguments[0]).readAsLines();
+  } else {
+    await for (final line in stdin.transform(utf8.decoder).transform(const LineSplitter())) {
+      lines.add(line);
+    }
+  }
+
+  solvePart1(lines);
+}
---
+++ test/aoc_2013_test.dart
@@ -0,0 +1,8 @@
+import 'package:aoc_2013/aoc_2013.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
